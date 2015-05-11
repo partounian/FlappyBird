297bb2e50c54e4559185d5c7a2ca5e90418c7539
Patrick Artounian <partounian@gmail.co>
2015-05-11 13:42:40 -0700

    Inital commit.

---
+++ run.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\Java\jdk1.7.0_67\bin\javac.exe" src/*.java
+java -cp src/ App
+rm src/*.class
---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd C:\Users\p26208\Dropbox\FlappyBird-master
+"C:\Program Files\Java\jdk1.7.0_67\bin\javac.exe" src/*.java
+java -cp src/ App
+rm src/*.class
