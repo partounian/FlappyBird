152a736173173c7cbc7ea31b5552146410e55219
Patrick Artounian <partounian@gmail.co>
2015-05-11 14:07:13 -0700

    No changes

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd C:\Users\p26208\Dropbox\FlappyBird-master
+cd C:\Users\p26208\Dropbox\FlappyBird
 "C:\Program Files\Java\jdk1.7.0_67\bin\javac.exe" src/*.java
 java -cp src/ App
 rm src/*.class
